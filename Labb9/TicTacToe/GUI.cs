543b020ef72407a6223d92f0ecca780406cf7e43
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2016-11-10 14:41:09 +0100

    Labb 8, 8 vg och labb 9

--- Labb9/TicTacToe/GUI.cs
+++ Labb9/TicTacToe/GUI.cs
@@ -9,6 +9,7 @@ namespace TicTacToe
 {
     class GUI
     {
+       
         public void GraficGrid(char[] arr)
         {
             Console.WriteLine("********************");
@@ -31,7 +32,7 @@ namespace TicTacToe
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Player 1 Chance");
-                
+
             }
             else
             {
@@ -65,17 +66,34 @@ namespace TicTacToe
         internal void PlayerWin(char[] arr, int player)
         {
             if ((player % 2) + 1 == 1)
-            { 
-            Console.ForegroundColor = ConsoleColor.Red;
-            GraficGrid(arr);// getting filled board again
-            Console.WriteLine("Player {0} has won", (player % 2) + 1);
-            Console.ResetColor();
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                GraficGrid(arr);// getting filled board again
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                    Thread.Sleep(700);
+                    ClearOneLine();
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                Thread.Sleep(200);
+                Console.ResetColor();
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 GraficGrid(arr);// getting filled board again
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                    Thread.Sleep(700);
+                    ClearOneLine();
+                    Thread.Sleep(500);
+                }
                 Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                Thread.Sleep(200);
                 Console.ResetColor();
             }
         }
@@ -86,6 +104,97 @@ namespace TicTacToe
             GraficGrid(arr);// getting filled board again
             Console.WriteLine("Draw");
             Console.ResetColor();
+        }
+
+        internal void BackOrExit()
+        {
+            var runtime = new Runtime();
+            Console.WriteLine("***************************");
+            Console.WriteLine("*   1. Start Game again   *");
+            Console.WriteLine("*   2. Exit               *");
+            Console.WriteLine("***************************");
+            var input = Console.ReadKey(true).Key;
+            switch (input)
+            {
+                case ConsoleKey.NumPad1:
+                case ConsoleKey.D1:
+                    
+                    break;
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        public int TakeChoice()//Taking users choice 
+        {
+
+            int x = 0;
+            bool loop = true;
+            while (loop)
+            {
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "2")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "3")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "4")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "5")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "6")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "7")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "8")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "9")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                else
+                    Console.WriteLine("Choose between 1-9");
+                Thread.Sleep(1000);
+                ClearOneLine();
+                ClearOneLine();
+            }
+
+            return x;
+        }
+
+        void ClearOneLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
         }
     }
 }
--- Labb9/TicTacToe/Runtime.cs
+++ Labb9/TicTacToe/Runtime.cs
@@ -13,26 +13,33 @@ namespace TicTacToe
         IBoard board = new Board();
         GUI gui = new GUI();
         int choice;
-        public static int flag = 0;
+        public static int flag { get; set; } = 0;
 
         public void Start()
         {
             Node grid = new Node();
-            int player = grid.Player=1;
+            int player = grid.Player = 1;
             char[] mark = grid.Mark;
-
+            
             do
             {
                 Console.Clear();
-                
-                gui.PlayerTurn(player);
-                gui.GraficGrid(mark);
-                choice = TakeChoice();
+                GraficGrid(mark,player);
+                choice = gui.TakeChoice();
                 board.PlaceMarker(choice, mark, player);// calling the PlaceMarker Function  
                 player++;
                 Console.ResetColor();
             } while (flag != 1 && flag != -1);// This loop will be run until all cell of the grid is not marked with X and O or some player is not win
             GameOver(mark,flag,player);
+        }
+
+
+
+
+        void GraficGrid(char[] mark, int player)
+        {
+            gui.PlayerTurn(player);
+            gui.GraficGrid(mark);
         }
 
         public static int CheckWin(char[] arr)
@@ -102,147 +109,51 @@ namespace TicTacToe
         void GameOver(char[] arr, int flag, int player)
         {
             Console.Clear();// clearing the console  
-              
-
-            if (flag == 1)// if flag value is 1 then someone won or means who played marked last time which has win  
+            var counter = new Counter() { WinWin = flag };
+            var counter2 = new Counter() { DrawDraw = flag };
+            counter.WinMessage += (sender, e) =>
             {
-                gui.PlayerWin(arr,player);
-            }
-            else// if flag value is -1 the match will be draw and no one is winner  
+                gui.PlayerWin(arr, player);
+                gui.BackOrExit();
+            };
+            counter2.DrawMessage += (sender, e) =>
             {
                 gui.PlayerDraw(arr, player);
-                
+                gui.BackOrExit();
+            };
+            if (counter.WinWin == 1)
+            {
+                counter.OnWinnigHappend(this, EventArgs.Empty);
             }
+            else if (counter.WinWin == -1)
+            {
+                counter.OnDrawHappend(this, EventArgs.Empty);
+            }
+
+
+
+
+            //if (flag==1)
+            //{                
+            //    gui.PlayerWin(arr, player);
+            //    gui.BackOrExit();
+            //}
+
+            //else// if flag value is -1 the match will be draw and no one is winner  
+            //{
+            //    gui.PlayerDraw(arr, player);
+            //    gui.BackOrExit();
+            //}
             Console.ReadLine();
 
         }
 
+        
+        
 
-        int TakeChoice()//Taking users choice 
-        {
+        
 
-            int x = 0;
-            bool loop = true;
-            while (loop)
-            {
-                Console.WriteLine("Choose between 1-9");
-                string input = Console.ReadLine();
-                if (input == "1")
-                { 
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "2")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "3")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "4")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "5")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "6")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "7")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "8")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                if (input == "9")
-                {
-                    x = Convert.ToInt16(input);
-                    loop = false;
-                }
-                else
-                    
-                Thread.Sleep(1200);
-                    ClearOneLine();
-                ClearOneLine();
-            }
-
-            #region switch
-            //var input = Console.ReadKey(true).Key;
-            //switch (input)
-            //{
-                //case ConsoleKey.NumPad1:
-                //case ConsoleKey.D1:
-                //    Console.WriteLine("1");
-                //    x = 1;
-                //    break;
-                //case ConsoleKey.NumPad2:
-                //case ConsoleKey.D2:
-                //    Console.WriteLine("2");
-                //    x = 2;
-                //    break;
-                //case ConsoleKey.NumPad3:
-                //case ConsoleKey.D3:
-                //    Console.WriteLine("3");
-                //    x = 3;
-                //    break;
-                //case ConsoleKey.NumPad4:
-                //case ConsoleKey.D4:
-                //    Console.WriteLine("4");
-                //    x = 4;
-                //    break;
-                //case ConsoleKey.NumPad5:
-                //case ConsoleKey.D5:
-                //    Console.WriteLine("5");
-                //    x = 5;
-                //    break;
-                //case ConsoleKey.NumPad6:
-                //case ConsoleKey.D6:
-                //    Console.WriteLine("6");
-                //    x = 6;
-                //    break;
-                //case ConsoleKey.NumPad7:
-                //case ConsoleKey.D7:
-                //    Console.WriteLine("7");
-                //    x = 7;
-                //    break;
-                //case ConsoleKey.NumPad8:
-                //case ConsoleKey.D8:
-                //    Console.WriteLine("8");
-                //    x = 8;
-                //    break;
-                //case ConsoleKey.NumPad9:
-                //case ConsoleKey.D9:
-                //    Console.WriteLine("9");
-                //    x = 9;
-                //    break;
-                //default:
-                //    Console.WriteLine("Choose between 1-9 plz");
-                //    break;
-                #endregion
-            
-
-            return x;
-        }
-        void ClearOneLine()
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-        }
-
+        
     }
 }
 
