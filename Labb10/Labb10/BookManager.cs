bc577b2e1414cf2fbc56ffac73d305ab20cb6113
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2016-11-14 11:20:50 +0100

    Labb10

---
+++ Labb10/Labb10/BookManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb10
+{
+    class BookManager
+    {
+        public List<Book> MyBooks { get; set; }
+
+        public BookManager()
+        {
+            MyBooks = new List<Book>
+            {
+                new Book {Title = "en bok som är helt värdelös", Genre = "Fiction", Pages = 500, Price = 2500 },
+                new Book {Title = "Kort historia", Genre = "ShortStories", Pages = 10, Price = 50 },
+                new Book {Title = "Danskarnas krig", Genre = "Novelbooks", Pages = 50, Price= 150},
+                new Book {Title = "Farliga vatten", Genre = "Romance", Pages = 200, Price = 100 },
+                new Book {Title = "Flygande Drake", Genre = "Mystery" , Pages = 340, Price = 500 },
+                new Book {Title = "Apan och Dansken",Genre = "Romance", Pages = 550, Price = 10 },
+                new Book {Title = "Dansken saknar Sverige", Genre = "Fantasy", Pages = 1000, Price = 1500}
+                
+            };
+
+        }
+
+        public void PrintWhere(BookFilter filter)
+        {
+            foreach (var book in MyBooks)
+            {
+                if(filter(book))
+                    Console.WriteLine(book.Title);
+            }
+        }
+        
+    }
+}
---
+++ Labb10/Labb10/Runtime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb10
+{
+    class Runtime
+    {
+
+        public void Start()
+        {
+            BookManager manager = new BookManager();
+
+            BookFilter IsNovelFilter = BookFilters.IsNovel;
+            BookFilter IsShortStoryFilter = BookFilters.IsShortStory;
+            BookFilter IsGenreMysteryFilter = BookFilters.IsGenreMystery;
+            BookFilter IsGenreFantasyFilter = BookFilters.IsGenreFantasy;
+            BookFilter IsGenreRomanceFilter = BookFilters.IsGenreRomance;
+            BookFilter IsCheapFilter = BookFilters.IsCheap;
+            BookFilter IsExpensiveFilter = BookFilters.IsExpensive;
+
+            Console.WriteLine("Novel: ");
+            manager.PrintWhere(IsNovelFilter);
+
+            Console.WriteLine("\nShort Story: ");
+            manager.PrintWhere(IsShortStoryFilter);
+
+            Console.WriteLine("\nMystery: ");
+            manager.PrintWhere(IsGenreMysteryFilter);
+
+            Console.WriteLine("\nFantasy: ");
+            manager.PrintWhere(IsGenreFantasyFilter);
+
+            Console.WriteLine("\nRomance: ");
+            manager.PrintWhere(IsGenreRomanceFilter);
+
+            Console.WriteLine("\n Cheap Books: ");
+            manager.PrintWhere(IsCheapFilter);
+
+            Console.WriteLine("\n Expensive books; ");
+            manager.PrintWhere(IsExpensiveFilter);
+        }
+    }
+}
